9fbf3921c958736e2fde501e734657ff5ec152b8
Matthew Drumheller <MattDrummy@gmail.com>
2018-01-26 11:17:02 -0600

    set url and reloads to true

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,18 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
+  host: `https://sheets.googleapis.com/v4/spreadsheets/1DedVf2UTdysxyEKoQpu1nWNttx_oK7iwKUpzWv2MrUs`,
+  shouldReloadRecord: function() {
+    return true;
+  },
+  shouldReloadAll: function() {
+    return true;
+  },
+  shouldBackgroundReloadRecord: function() {
+    return true;
+  },
+  shouldBackgroundReloadAll: function() {
+    return true;
+  },
+  
 });
