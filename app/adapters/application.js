36c21293c7a702502855dab29d2d2f5424b35b1c
Matthew Drumheller <MattDrummy@gmail.com>
2018-01-26 12:21:14 -0600

    linked adapter and model to google sheets api

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,9 @@
 import DS from 'ember-data';
+import $ from 'jquery'
+import { Promise } from 'rsvp'
 
-export default DS.JSONAPIAdapter.extend({
-  host: `https://sheets.googleapis.com/v4/spreadsheets/1DedVf2UTdysxyEKoQpu1nWNttx_oK7iwKUpzWv2MrUs`,
+export default DS.RESTAdapter.extend({
+  host: `https://sheets.googleapis.com/v4/spreadsheets/1DedVf2UTdysxyEKoQpu1nWNttx_oK7iwKUpzWv2MrUs/values/Sheet1!A1:C2?key=AIzaSyAqR1RPBQfCdYsJC6ojsZ2J25smy4iFO4I`,
   shouldReloadRecord: function() {
     return true;
   },
@@ -14,5 +16,27 @@ export default DS.JSONAPIAdapter.extend({
   shouldBackgroundReloadAll: function() {
     return true;
   },
-  
+  findAll(store, type, sinceToken){
+    let query = { since: sinceToken };
+    let host = this.get('host')
+
+    return new Promise(function(resolve, reject){
+      $.getJSON(host, query).then(function(data){
+        let mapping = {
+          "comics": data.values.map((e,i)=>{
+            return {
+              id: i,
+              url: e[0],
+              comment: e[1],
+              title: e[2],
+            }
+          })
+        }
+        console.log(mapping);
+        resolve(mapping)
+      }, function(err){
+        reject(err)
+      })
+    })
+  },
 });
--- app/models/comic.js
+++ app/models/comic.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  url: DS.attr(),
+  comment: DS.attr(),
+  title: DS.attr(),
 });
